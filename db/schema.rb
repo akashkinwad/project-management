01a5fd728975a158e7ad5f5d342eb961555a7c10
Akash Kinwad <akashkinwad@gmail.com>
2019-09-14 13:58:29 +0530

    added name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_033847) do
+ActiveRecord::Schema.define(version: 2019_10_10_033848) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_10_10_033847) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
